1e07523e77ea665046fbaa93db095e4214673bf7
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2016-02-04 23:37:47 +0100

    Updated Copyright years to 2016

--- magnolia-blossom-sample-webapp/src/main/java/info/magnolia/blossom/sample/service/SalesApplicationWebServiceImpl.java
+++ magnolia-blossom-sample-webapp/src/main/java/info/magnolia/blossom/sample/service/SalesApplicationWebServiceImpl.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2010-2015 Magnolia International
+ * This file Copyright (c) 2010-2016 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
