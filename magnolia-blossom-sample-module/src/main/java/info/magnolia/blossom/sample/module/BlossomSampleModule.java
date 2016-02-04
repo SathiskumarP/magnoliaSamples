c3a6362fcd11505388bbb14d3d4696ed8bc6c6ac
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2016-02-04 23:37:49 +0100

    Updated Copyright years to 2016

--- magnolia-blossom-sample-module/src/main/java/info/magnolia/blossom/sample/module/BlossomSampleModule.java
+++ magnolia-blossom-sample-module/src/main/java/info/magnolia/blossom/sample/module/BlossomSampleModule.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2011-2015 Magnolia International
+ * This file Copyright (c) 2011-2016 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
