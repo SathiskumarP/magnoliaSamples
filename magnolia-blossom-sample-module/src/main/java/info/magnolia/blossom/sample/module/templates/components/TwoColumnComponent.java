ecc0703ef3b01dc867099d88e91e02ee960e5070
Mikaël Geljić <mikael.geljic@magnolia-cms.com>
2017-01-03 00:39:59 +0100

    Update Copyright years to 2017

--- magnolia-blossom-sample-module/src/main/java/info/magnolia/blossom/sample/module/templates/components/TwoColumnComponent.java
+++ magnolia-blossom-sample-module/src/main/java/info/magnolia/blossom/sample/module/templates/components/TwoColumnComponent.java
@@ -1,5 +1,5 @@
 /**
- * This file Copyright (c) 2013-2016 Magnolia International
+ * This file Copyright (c) 2013-2017 Magnolia International
  * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
  *
  *
